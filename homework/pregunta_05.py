3f87d65c2daa9b5bd025ef833b77b9e176eafd41
Santiago Bustos Pianda <119011448+piandas@users.noreply.github.com>
2025-05-20 22:48:04 -0500

    5 hecha

--- homework/pregunta_05.py
+++ homework/pregunta_05.py
@@ -5,6 +5,7 @@ solo puede utilizar las funciones y librerias basicas de python. No puede
 utilizar pandas, numpy o scipy.
 """
 
+import csv
 
 def pregunta_05():
     """
@@ -15,3 +16,31 @@ def pregunta_05():
     [('A', 9, 2), ('B', 9, 1), ('C', 9, 0), ('D', 8, 3), ('E', 9, 1)]
 
     """
+
+    counts = {}
+    
+    with open('files/input/data.csv') as f:
+        reader = csv.reader(f, delimiter='\t')
+        
+        counts = {}
+        
+        for row in reader:
+            letra = row[0]
+            valor = int(row[1])
+            
+            if letra not in counts:
+                counts[letra] = [valor]
+            else:
+                counts[letra].append(valor)
+                
+    resultado = []
+    for letra in sorted(counts):
+        lista_valores = counts[letra]
+        maximo = max(lista_valores)
+        minimo = min(lista_valores)
+        resultado.append((letra, maximo, minimo))
+        
+    return resultado
+
+resultado = pregunta_05()
+print(resultado)
